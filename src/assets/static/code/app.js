792649868f413b2a365776d4ea1b27fdba4808ed
PedroBarr <pebarreraa@udistrital.edu.co>
2024-07-10 23:21:53 -0500

    Corregir entorno

--- src/assets/static/code/app.js
+++ src/assets/static/code/app.js
@@ -8,9 +8,9 @@ let enviroment = 'prod';
 if (localHosts.find(item => window.location.hostname.includes(item)))
   enviroment = 'dev';
 
-export const api = !(enviroment === 'prod') ?
+export const api = (enviroment === 'prod') ?
   'https://ekcion-api.up.railway.app/api' : 'http://localhost:8000/api';
-export const assets = !(enviroment === 'prod') ?
+export const assets = (enviroment === 'prod') ?
   'https://ekcion-api.up.railway.app/assets' : 'http://localhost:8000/assets';
 export const base = (enviroment === 'prod') ?
   '/ekmajstro_pre_cion_frontend' : '';
